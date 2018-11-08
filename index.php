3cc0d58b3211f1cd975e3c37889c049768faf50b
tommyjakobsen <tommy.jakobsen@outlook.com>
2018-11-08 11:38:39 +0100

    Fetching env variables in openShift
    Env variables for the AI binding in Azure

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
+<?php
 
+
+echo "<html>
+ <head>
+   <title>Azure AI from openShift</title>
+ </head>
+<body>";
+
+
+if(getenv('aiEndPoint') !== false)
+    {
+        $aiEndPoint=getenv('aiEndPoint');
+        echo "Found AI endpoint at: $aiEndPoint<hr>";
+     }else{
+        echo "<font color=red><b>! \$aiEndPoint variable not set...<br>Check bindings in openshift...</font>";
+        echo "</body>";
+     exit(1);
+     }
+
+if(getenv('aiKey') !== false)
+    {
+        $aiKey=getenv('aiKey');
+        echo "Found aiKey at: $aiKey<hr>";
+     }else{
+        echo "<font color=red><b>! \$aiKey variable not set...<br>Check bindings in openshift...</font>";
+        echo "</body>";
+     exit(1);
+     }
+
+
+if(getenv('aiName') !== false)
+    {
+        $aiName=getenv('aiName');
+        echo "Found aiName at: $aiName<hr>";
+     }else{
+        echo "<font color=red><b>! \$aiName variable not set...<br>Check bindings in openshift...</font>";
+        echo "</body>";
+     exit(1);
+     }
+
+
+
+
+echo "</body>";
