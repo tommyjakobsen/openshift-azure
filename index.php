f5f192cb1198fa0a14ca74cae4ba787fa999858e
tommyjakobsen <tommy.jakobsen@outlook.com>
2018-11-09 11:04:27 +0100

    Added function and layout
    Added http-function and changed the layout.
    Need to add sentiment to the layout

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
 
 
-echo "<html>
+echo "<!DOCTYPE html>
+<html>
  <head>
    <title>Azure AI from openShift</title>
+
+<style>
+#rcorners1 {
+    border-radius: 25px;
+    background: #73AD21;
+    padding: 20px; 
+    width: 600px;
+    height: 150px;    
+}
+
+#rcorners2 {
+    border-radius: 25px;
+    border: 2px solid #73AD21;
+    padding: 20px; 
+    width: 200px;
+    height: 150px;    
+}
+</style>
  </head>
 <body>";
 
@@ -69,28 +88,48 @@ $jsondata='{
 
 echo "<b>Endpoint: </b>${textAnalyticsEndpoint}/languages<br>";
 echo "<b>AppName: </b>$textAnalyticsName<br>";
+function http_request($url, $textAnalyticsKey,  $jsondata){	
+	//Initiate cURL.
+	$ch = curl_init($url);
+ 	
+	//Tell cURL that we want to send a POST request.
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ 	
+	//Attach our encoded JSON string to the POST fields.
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsondata);
+ 	
+	//Set the content type to application/json
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Ocp-Apim-Subscription-Key: $textAnalyticsKey", "Content-Type: application/json")); 
+ 	
+	//Execute the request
+	echo "<br><br>";
+	$output=curl_exec($ch);
+	curl_close($ch);
+	return  $output;
+}
 
-
-echo preg_replace('/\n/', '<br>', $jsondata);
-
-echo "<hr><br><br><b>Result:<br>"; 
-//API Url
 $url = "${textAnalyticsEndpoint}/languages";
- 
-//Initiate cURL.
-$ch = curl_init($url);
- 
-//Tell cURL that we want to send a POST request.
-curl_setopt($ch, CURLOPT_POST, 1);
- 
-//Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsondata);
- 
-//Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("Ocp-Apim-Subscription-Key: $textAnalyticsKey", "Content-Type: application/json")); 
- 
-//Execute the request
-echo "<br><br>";
-curl_exec($ch);
+$output=http_request($url, $textAnalyticsKey, $jsondata);
+$url = "${textAnalyticsEndpoint}/sentiment";
+$sentiments=json_decode(http_request($url, $textAnalyticsKey, $jsondata));
 
-echo "</body>";
+$postdata=json_decode($jsondata);
+$result=json_decode($output);
+
+echo "<h1>Azure Analytics - Language</h1>";
+foreach($postdata->documents as $key=>$val)
+	{
+
+	echo "<p id='rcorners1'>".$val->text."<br>";
+	$id=$val->id;
+	echo "<br><br><br><b>Language: </b>".$result->documents[$key]->detectedLanguages[0]->name."<br>";
+	echo "<b>ISO:</b> ".$result->documents[$key]->detectedLanguages[0]->iso6391Name."<br>";
+	$score=$result->documents[$key]->detectedLanguages[0]->score;
+	echo "<b>Certainty: </b>".($score*100)."%</p>";
+
+	}
+
+
+
+echo "</body></html>";
