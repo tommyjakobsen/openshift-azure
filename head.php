169de724b41053223c4ade6949890f1cb503234a
tommyjakobsen <tommy.jakobsen@outlook.com>
2018-11-12 15:01:16 +0100

    Create head.php

---
+++ head.php
@@ -0,0 +1,35 @@
+<?php
+echo "<!DOCTYPE html>
+<html>
+ <head>
+   <title>Azure AI from openShift</title>
+<style>
+a:link {
+    text-decoration: none;
+}
+a:visited {
+    text-decoration: none;
+}
+a:hover {
+    text-decoration: underline;
+}
+a:active {
+    text-decoration: underline;
+}
+#rcorners1 {
+    border-radius: 25px;
+    background: #bcd698;
+    padding: 20px; 
+    width: 600px;
+    height: 150px;    
+}
+#rcorners2 {
+    border-radius: 25px;
+    border: 2px solid #73AD21;
+    padding: 20px; 
+    width: 200px;
+    height: 150px;    
+}
+</style>
+ </head>
+<body>";
